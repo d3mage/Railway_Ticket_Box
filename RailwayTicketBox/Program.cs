5ec75835bd785fee4726b0ded57406a2284ccdb3
d3mage <d3magexgod@gmail.com>
2021-05-26 00:06:33 +0300

    Setup project for starting

--- RailwayTicketBox/Program.cs
+++ RailwayTicketBox/Program.cs
@@ -1,4 +1,11 @@
-﻿using PL; 
+﻿using PL;
+using BLL;
+using BLL.TrainService;
+using BLL.CarService;
+using DAL.Entities;
+using BLL.Entities;
+using DAL; 
+using DAL.Provider; 
 
 namespace RailwayTicketBox
 {
@@ -6,7 +13,18 @@ namespace RailwayTicketBox
     {
         static void Main(string[] args)
         {
-            Menu menu = new Menu(); 
+            XmlProvider<TrainDAL> traimXML = new();
+            DataContext<TrainDAL> trainContext = new(traimXML, "train.xml");
+            ReadWriteService<Train, TrainDAL> trainRW = new(trainContext);
+            TrainService trainService = new TrainService(trainRW);
+            
+            XmlProvider<CarDAL> carXML = new();
+            DataContext<CarDAL> carContext = new(carXML, "car.xml");
+            ReadWriteService<Car, CarDAL> carRW = new(carContext);
+            CarService carService = new CarService(carRW);
+
+
+            Menu menu = new Menu(trainService, carService); 
         }
     }
 }

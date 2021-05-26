947714d1eeafe9427e1263769d0d14ff2fd21973
d3mage <d3magexgod@gmail.com>
2021-05-26 03:26:35 +0300

    New BLL exceptions

---
+++ BLL/Exception/BookingNumberException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BookingNumberException : Exception
+    {
+        public override string Message => "Entered booking number is invalid.";
+    }
+}
---
+++ BLL/Exception/CarNumberException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BLL
+{ 
+    public class CarNumberException : Exception
+    {
+        public override string Message => "Entered car number is invalid.";
+    }
+}

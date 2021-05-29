9132397a517ff13fec3f9c52288d036ba3d8b7d2
d3mage <d3magexgod@gmail.com>
2021-05-29 21:34:31 +0300

    Updated Conversion service for other entities

--- BLL/ConversionService/ConversionService.cs
+++ BLL/ConversionService/ConversionService.cs
@@ -4,32 +4,76 @@ using DAL.Entities;
 
 namespace BLL.ConversionService
 {
-    static class ConversionService<T,Y> 
+    public static class ConversionService<T,Y> 
     {
         public static void conversion(List<T> convertive, List<Y> converted)
         {
-            if(convertive is List<Train> trains && converted is List<TrainDAL> trainsDAL)
+            if (convertive is List<Train> trains && converted is List<TrainDAL> trainsDAL)
             {
-                conversionTrainToDAL(trains, trainsDAL); 
+                conversionTrainToDAL(trains, trainsDAL);
             }
-            else if(convertive is List<TrainDAL> trains1DAL  && converted is List<Train> trains1)
+            else if (convertive is List<TrainDAL> trains1DAL && converted is List<Train> trains1)
             {
                 conversionDALToTrain(trains1DAL, trains1);
             }
+            else if (convertive is List<Car> cars && converted is List<CarDAL> carsDAL)
+            {
+                conversionCarToDAL(cars, carsDAL);
+            }
+            else if (convertive is List<CarDAL> cars1DAL && converted is List<Car> cars1)
+            {
+                conversionDALToCar(cars1DAL, cars1);
+            }
+            else if (convertive is List<Booking> bookings && converted is List<BookingDAL> bookingsDAL)
+            {
+                conversionBookingToDAL(bookings, bookingsDAL);
+            }
+            else if (convertive is List<BookingDAL> bookings1DAL && converted is List<Booking> bookings1)
+            {
+                conversionDALToBooking(bookings1DAL, bookings1);
+            }
         }
 
-        public static void  conversionTrainToDAL(List<Train> trains, List<TrainDAL> trainsDAL)
+        private static void  conversionTrainToDAL(List<Train> trains, List<TrainDAL> trainsDAL)
         {
             foreach(Train train in trains)
             {
                 trainsDAL.Add(new TrainDAL(train.trainNumber, train.dispatch, train.destination, train.departure));
             }
         }
-        public static void  conversionDALToTrain(List<TrainDAL> trainsDAL, List<Train> trains)
+        private static void  conversionDALToTrain(List<TrainDAL> trainsDAL, List<Train> trains)
         {
             foreach(TrainDAL train in trainsDAL)
             {
                 trains.Add(new Train(train.trainNumber, train.dispatch, train.destination, train.departure));
+            }
+        }
+        private static void  conversionCarToDAL(List<Car> cars, List<CarDAL> carsDAL)
+        {
+            foreach(Car car in cars)
+            {
+                carsDAL.Add(new CarDAL(car.trainNumber, car.carNumber, car.sitsTaken));
+            }
+        }
+        private static void  conversionDALToCar(List<CarDAL> carsDAL, List<Car> cars)
+        {
+            foreach(CarDAL car in carsDAL)
+            {
+                cars.Add(new Car(car.trainNumber, car.carNumber, car.sitsTaken));
+            }
+        }
+        private static void  conversionBookingToDAL(List<Booking> bookings, List<BookingDAL> bookingsDAL)
+        {
+            foreach(Booking booking in bookings)
+            {
+                bookingsDAL.Add(new BookingDAL(booking.trainNumber, booking.carNumber, booking.sitNumber));
+            }
+        }
+        private static void  conversionDALToBooking(List<BookingDAL> bookingsDAL, List<Booking> bookings)
+        {
+            foreach(BookingDAL booking in bookingsDAL)
+            {
+                bookings.Add(new Booking(booking.trainNumber, booking.carNumber, booking.sitNumber));
             }
         }
     }
---
+++ Tests/BLL.Tests/conversionService.Tests.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using BLL.Entities;
+using DAL.Entities;
+using BLL.ConversionService;
+namespace Tests.BLL.Tests
+{
+    public class conversionService
+    {
+        [Fact]
+        public void convert_TrainToDAL()
+        {
+            List<Train> trains = testData.GetTrainList();
+            List<Train> expected = trains;
+            List<TrainDAL> actual = new();
+
+            ConversionService<Train, TrainDAL>.conversion(trains, actual);
+
+            Assert.NotNull(actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].departure, actual[i].departure);
+                Assert.Equal(expected[i].departure, actual[i].departure);
+            }
+        }
+        [Fact]
+        public void convert_DALToTrain()
+        {
+            List<TrainDAL> trains = testData.GetTrainDALList();
+            List<TrainDAL> expected = trains;
+            List<Train> actual = new();
+
+            ConversionService<TrainDAL, Train>.conversion(trains, actual);
+
+            Assert.NotNull(actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].departure, actual[i].departure);
+                Assert.Equal(expected[i].departure, actual[i].departure);
+            }
+        }
+        [Fact]
+        public void convert_CarToDAL()
+        {
+            List<Car> cars = testData.GetCarList();
+            List<Car> expected = cars;
+            List<CarDAL> actual = new();
+
+            ConversionService<Car, CarDAL>.conversion(cars, actual);
+
+            Assert.NotNull(actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].trainNumber, actual[i].trainNumber);
+                Assert.Equal(expected[i].carNumber, actual[i].carNumber);
+                Assert.Equal(expected[i].sitsTaken, actual[i].sitsTaken);
+            }
+        }
+        [Fact]
+        public void convert_DALToCar()
+        {
+            List<CarDAL> trains = testData.GetCarDALList();
+            List<CarDAL> expected = trains;
+            List<Car> actual = new();
+
+            ConversionService<CarDAL, Car>.conversion(trains, actual);
+
+            Assert.NotNull(actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].trainNumber, actual[i].trainNumber);
+                Assert.Equal(expected[i].carNumber, actual[i].carNumber);
+                Assert.Equal(expected[i].sitsTaken, actual[i].sitsTaken);
+            }
+        }
+        [Fact]
+        public void convert_BookingToDAL()
+        {
+            List<Booking> trains = testData.GetBookingList();
+            List<Booking> expected = trains;
+            List<BookingDAL> actual = new();
+
+            ConversionService<Booking, BookingDAL>.conversion(trains, actual);
+
+            Assert.NotNull(actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].trainNumber, actual[i].trainNumber);
+                Assert.Equal(expected[i].carNumber, actual[i].carNumber);
+                Assert.Equal(expected[i].sitNumber, actual[i].sitNumber);
+            }
+        }
+        [Fact]
+        public void convert_DALToBooking()
+        {
+            List<BookingDAL> trains = testData.GetBookingDALList();
+            List<BookingDAL> expected = trains;
+            List<Booking> actual = new();
+
+            ConversionService<BookingDAL, Booking>.conversion(trains, actual);
+
+            Assert.NotNull(actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].trainNumber, actual[i].trainNumber);
+                Assert.Equal(expected[i].carNumber, actual[i].carNumber);
+                Assert.Equal(expected[i].sitNumber, actual[i].sitNumber);
+            }
+        }
+    }
+}
+
+

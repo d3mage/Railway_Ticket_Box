e63c59c1b9cada94da67bccd25b68758fcafcace
d3mage <d3magexgod@gmail.com>
2021-05-30 03:37:40 +0300

    Created BookingService

---
+++ BLL/BookingService/BookingService.cs
@@ -0,0 +1,61 @@
+﻿using BLL;
+using System.Collections.Generic;
+using BLL.Entities;
+using DAL.Entities;
+using System;
+using System.Text;
+
+namespace BLL.BookingService
+{
+    public class BookingService : IBookingService
+    {
+        IReadWriteService<Booking, BookingDAL> readWrite;
+
+        public BookingService(IReadWriteService<Booking, BookingDAL> readWrite)
+        {
+            this.readWrite = readWrite; 
+        }
+
+        public void add(ulong train, ushort car, byte sit)
+        {
+            Booking added = new Booking(train, car, sit);
+            List<Booking> bookings = readWrite.ReadData();
+            bookings.Add(added);
+            readWrite.WriteData(bookings);
+        }
+
+        public void delete(ulong train, ushort car, byte sit)
+        {
+            List<Booking> bookings = readWrite.ReadData();
+            bookings.Remove(bookings.Find(x => x.trainNumber == train && x.carNumber == car));
+            readWrite.WriteData(bookings);
+        }
+
+        public void bookingExists(ulong train, ushort car, byte sit, bool shouldExist)
+        {
+            List<Booking> bookings = readWrite.ReadData();
+            Booking single = bookings.Find(x => x.trainNumber == train && x.carNumber == car && x.sitNumber == sit);
+            if (single == null && shouldExist == true) throw new BookingNumberException();
+        }
+
+        public string getBooking(ulong train, ushort car, byte sit)
+        {
+            List<Booking> bookings = readWrite.ReadData();
+            Booking single = bookings.Find(x => x.trainNumber == train && x.carNumber == car && x.sitNumber == sit);
+            return single.ToString(); 
+        }
+
+        public string getBookingByDate(DateTime dateTime)
+        {
+            List<Booking> bookings = readWrite.ReadData();
+            bookings = bookings.FindAll(x => x.reservationDate == dateTime);
+            StringBuilder builder = new StringBuilder(); 
+            foreach(Booking booking in bookings)
+            {
+                builder.Append(booking.ToString());
+                builder.Append("\n");
+            }
+            return builder.ToString(); 
+        }
+    }
+}
---
+++ BLL/BookingService/IBookingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BookingService
+{
+    public interface IBookingService
+    {
+        public void add(ulong train, ushort car, byte sit);
+        public void delete(ulong train, ushort car, byte sit);
+        public void bookingExists(ulong train, ushort car, byte sit, bool shouldExist);
+        public string getBooking(ulong train, ushort car, byte sit);
+        public string getBookingByDate(DateTime dateTime);
+    }
+}
